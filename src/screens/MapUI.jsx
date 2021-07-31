7cbd4e792cab9a55c759f4ff246a487a0b31b72c
Ahmed Bahaa <ahmedbahaa.3@hotmail.com>
2021-07-31 14:48:21 +0200

    Adapted map size flexibility

--- src/screens/MapUI.jsx
+++ src/screens/MapUI.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./MapUI.css";
 import L from "leaflet";
 import axios from "axios";
@@ -7,6 +7,9 @@ import * as turf from "@turf/turf";
 var convert = require("xml-js");
 
 const MapUI = () => {
+
+  const [mapWidth,setMapWidth] = useState('100%')
+
   useEffect(() => {
     initMap();
   }, []);
@@ -70,6 +73,27 @@ const MapUI = () => {
         console.log(regions);
 
         var map = L.map("mapid");
+
+
+        var latlngs = [
+          [31.29732799140426, 34.2333984375],
+          [31.89621446335144, 24.80712890625],
+          [22.004174972902003, 24.98291015625],
+          [22.004174972902003, 36.8701171875],
+          [31.297327991404266, 34.2333984375]
+        ];
+
+        var features = turf.points(latlngs);
+
+        var center = turf.center(features);
+
+        map.setView(center["geometry"]["coordinates"], 13);
+
+        var egypt = L.polygon(latlngs, { fillOpacity: 0 }).addTo(map);
+
+        map.fitBounds(egypt.getBounds());
+
+
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
           maxZoom: 19,
           attribution:
@@ -80,42 +104,41 @@ const MapUI = () => {
           zoomOffset: -1
         }).addTo(map);
 
-        map.setView([regions[0]["yCenter"], regions[0]["xCenter"]], 13);
-
-        // var area = L.circle([regions[0]["yCenter"], regions[0]["xCenter"]], {
-        //   radius: regions[0]["radius"]
-        // }).addTo(map);
-
         regions.map(region => {
-            L.tileLayer
-            .wms("https://eos.com/landviewer/wms/7f609ae3-ffb8-4fd4-bdbc-7a295800990b", {
-              transparent: true,
-              layers: region["days"][0],
-              format: "image/png",
-              uppercase: true,
-              version: "1.3.0"
-            })
+          L.tileLayer
+            .wms(
+              "https://eos.com/landviewer/wms/7f609ae3-ffb8-4fd4-bdbc-7a295800990b",
+              {
+                transparent: true,
+                layers: region["days"][0],
+                format: "image/png",
+                uppercase: true,
+                version: "1.3.0"
+              }
+            )
             .addTo(map);
-            
-          });
 
-        var latlngs = [
-          [31.29732799140426, 34.2333984375],
-          [31.89621446335144, 24.80712890625],
-          [22.004174972902003, 24.98291015625],
-          [22.004174972902003, 36.8701171875],
-          [31.297327991404266, 34.2333984375]
-        ];
+          var circle = L.circle([region["yCenter"], region["xCenter"]], {
+            radius: region["radius"],
+            color: 'white',
+            fillOpacity: 0.3
+          }).addTo(map).on('click',function(e) {
+            // map.setView(e.latlng,10);
+            setTimeout(function(){ map.invalidateSize()}, 400);
+            setMapWidth('30%')
+            map.fitBounds(circle.getBounds(),{
+              padding: [30,30]
+            });
+            map.panTo(new L.LatLng(region["yCenter"], region["xCenter"]));
+          }).bindPopup('<h1>'+region["name"]+'</h1>');
 
-        var egypt = L.polygon(latlngs, { "fillOpacity": 0 }).addTo(map);
-
-        map.fitBounds(egypt.getBounds(), {
-          padding: [10, 10]
         });
+
+
       });
   };
 
-  return <div id="mapid"></div>;
+  return <div id="mapid" style={{width: mapWidth}} ></div>;
 };
 
 export default MapUI;
